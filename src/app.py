4eb10fd41f8bf9ad63bbe715872e7b2467f2b996
Tshepo Lepono <tshepol@letshego.com>
2025-09-08 19:49:38 +0200

    first commit

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+from datetime import datetime
+import socket
+app = Flask(__name__)
+
+@app.route("/api/v1/details")
+def details():
+    return jsonify({
+        'time' : datetime.now(),
+        'hostname' : socket.gethostname()
+    })
+
+
+@app.route("/api/v1/healthz")
+def health():
+    return jsonify({'status' : 'UP'}), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
+
+
+# /api/v1/details
+# /api/v1/healthz
+
