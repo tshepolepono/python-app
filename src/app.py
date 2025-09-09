0147b72299d025b97a108da823f397bb0f34d8bd
Tshepo Lepono <tshepol@letshego.com>
2025-09-09 07:25:24 +0200

    Add CI workflow

--- src/app.py
+++ src/app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 def details():
     return jsonify({
         'time' : datetime.now(),
-        'hostname' : socket.gethostname()
+        'hostname' : socket.gethostname(),
+        'message' : 'Sure sure Mtshepula!!'
     })
 
 
